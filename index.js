91f33b3b00a5fcfe75041f7ed1b4e9ab6cfb3a70
lexabexa <lexabexa3@gmail.com>
2023-09-28 01:14:40 -0700

    updates to files

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const questions = [
         type: "input",
         name: "Text",
         message: "Enter 1-3 Characters:",
+        validate: function (text) {
+            return text.length <= 3 && text.length > 0;
+        },
     },
     {
         type: "input",
@@ -16,64 +19,63 @@ const questions = [
         message: "Enter Color of TEXT",
     },
     {
+        type: "list",
+        name: "Shape",
+        message: "Choose Shape:",
+        choices: ["Square", "Triangle", "Circle"],
+    },
+    {
         type: "input",
         name: "Shape-color",
         message: "Enter Color of SHAPE",
     },
-    {
-        type: "input",
-        name: "Shape",
-        message: "Choose Shape:",
-        choices: ["Square","Triangle","Circle"],
-    },
-]
-//function app
-async function run() {
+];
+
+async function init() {
     try {
         const answers = await inquirer.prompt(questions); //asks ?s
 
-        const text = answers.Text;
         const textColor = answers["Text-color"];
-        const shapeColor = answers["Shape-color"]
+        const shapeColor = answers["Shape-color"];
         const Shape = answers.Shape;
 
         let shape; ///create new shapes
-        switch (Shape){
+        switch (Shape) {
             case "Square":
-                shape = new Square(); 
+                shape = new Square();
                 break;
             case "Circle":
-                shape = new Circle(); 
+                shape = new Circle();
                 break;
             case "Triangle":
-                shape = new Triangle(); 
+                shape = new Triangle();
                 break;
             default:
                 console.log("Not Valid Shape");
-                return;   
+                return;
         }
-//set shape color
-        shape.setColor(shapeColor); 
-//use input to generate SVG
-        const logoSVG = generatedSVG(shape, textColor, text);
- //save file as SVG
- fs.writeFileSync("logo.svg", logoSVG); 
- console.log("Logo.SVG Saved");
+        //set shape color
+        shape.setColor(shapeColor);
+        //use input to generate SVG
+        const logoSVG = generatedSVG(shape, textColor, answers.Text);
+        //save file as SVG
+        fs.writeFileSync("logo.svg", logoSVG);
+        console.log("Logo.SVG Saved");
     } catch (error) {
         console.error("Error", error);
-    } 
-}     
+    }
+}
 
 function generatedSVG(shape, textColor, text) {
     const shapeSVG = shape.render();
-    const textSVG = '<text y="140" x="150" font-size"50" text-anchor="middle" fill="${textColor}">${text}</text>';
-const SVG = `
-    <SVG height="300" width="300" xmlns="http://www.w3.org/2000/svg">
-            ${shapeSVG}
-            ${textSVG}
-    </SVG>
-    `;
+    const textSVG = `<text y="140" x="150" font-size="50" text-anchor="middle" fill="${textColor}">${text}</text>`;
+    const SVG = `
+        <SVG height="300" width="300" xmlns="http://www.w3.org/2000/svg">
+                        ${shapeSVG}
+                        ${textSVG}
+        </SVG>
+        `;
     return SVG;
 }
 
-run();
+init();
